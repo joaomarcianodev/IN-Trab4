692f33a2ca295c952de0ab245bb8e4886bcf7ebc
JoaoAugusto2020 <joaoaugustoptc2020@gmail.com>
2025-07-08 08:20:15 -0300

    first commit

---
+++ IN-Trab4/src/test/java/com/joao/IN_Trab4/InTrab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joao.IN_Trab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InTrab4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
